7281033b5bb950d427fd43babc7d4759892deb4e
MR BROWNWOLF <alrabbiabdullah2@gmail.com>
2022-08-16 18:41:57 +0600

    done

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+// type js start
+var typed = new Typed(".auto_type", {
+    strings: ["DESIGNER", "DEVELOPER", "FREELANCER"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
+// type js end
+
+// mixit part start
+var mixer = mixitup('.mixit');
+// mixit part end
+
+// lightbox part start
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+})
+// lightbox part end
+
+
+// wow part start
+new WOW().init();
+// wow part end
